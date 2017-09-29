c838979224918f94fac49120ce630621344ceb84
tommy19970714 <toshiki0714@icloud.com>
2017-09-29 13:38:25 +0900

    first commit

---
+++ Assets/Script/swim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swim : MonoBehaviour {
+
+	public GameObject canue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (1f, 2f, 0f));
+		Vector3 canuePosition = canue.transform.position;
+		canuePosition.y += 0.9f;
+		transform.position = canuePosition;
+	}
+}
