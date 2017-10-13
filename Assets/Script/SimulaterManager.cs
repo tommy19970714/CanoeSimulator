70a00f0747599a59054d1f61839cc1a2e98c7cb4
tommy19970714 <toshiki0714@icloud.com>
2017-10-13 13:26:50 +0900

    パドルの沈みを検知

---
+++ Assets/Script/SimulaterManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulaterManager : MonoBehaviour {
+
+    public GameObject canue;
+    public GameObject paddle;
+
+    public StayJudge stayJudge;
+
+    private Paddle paddle_right;
+    private Paddle paddle_left;
+
+    private float flowLevel = 0;
+    private Vector3 paddleAngle = new Vector3();
+
+    struct Paddle {
+        GameObject self;
+        Vector3 position;
+        Vector3 beforePosition;
+        int sinkLevel;
+
+        void update(GameObject paddleObject)
+        {
+            self = paddleObject;
+            next(self.transform.position);
+        }
+
+        void next(Vector3 p)
+        {
+            beforePosition = position;
+            position = p;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        paddleAngle = paddle.transform.rotation.eulerAngles;
+	}
+}
---
+++ Assets/Script/StayJudge.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayJudge : MonoBehaviour
+{
+    public int rightSinkCounter = 0;
+    public int leftSinkCounter = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("right" + rightSinkCounter.ToString());
+        //Debug.Log("left" + leftSinkCounter.ToString());
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        //Debug.Log("stay");
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Paddle_right"))
+        {
+            rightSinkCounter++;
+        }
+        else if (other.gameObject.CompareTag("Paddle_left"))
+        {
+            leftSinkCounter++;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Paddle_right"))
+        {
+            rightSinkCounter--;
+        }
+        else if (other.gameObject.CompareTag("Paddle_left"))
+        {
+            leftSinkCounter--;
+        }
+    }
+}
