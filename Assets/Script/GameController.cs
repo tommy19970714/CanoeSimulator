d0ed25266edd01dbb4d0c3704e2f8182fd609a0a
tommy19970714 <toshiki0714@icloud.com>
2017-10-20 13:44:58 +0900

    ゲーム用のコントローラーを作成

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public int polecounter;
+	public Text countLabel;
+	public Text timerLabel;
+	private List<int> idList = new List<int>();
+	public float timer = 0;
+	public int LimitTime = 30;
+
+	// Use this for initialization
+	void Start () {
+		countLabel.text = "0";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		int remainTime = LimitTime - (int)timer;
+		if(remainTime < 0) {
+			Application.LoadLevel("EndSene");
+		}
+		timerLabel.text = remainTime.ToString ();
+	}
+
+	public void addCounter(int id) {
+		if (idList.FindAll (x => x == id).Count == 0) {
+			polecounter += 1;
+			idList.Add (id);
+		}
+		countLabel.text = polecounter.ToString ();
+	}
+}
---
+++ Assets/Script/PoleJudge.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleJudge : MonoBehaviour {
+
+	public GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Canoe"))
+		{
+			gameController.addCounter (this.GetInstanceID());
+			Debug.Log (this.GetInstanceID());
+		}
+	}
+}
