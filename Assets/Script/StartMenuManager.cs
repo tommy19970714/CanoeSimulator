0e6321593d884ac38c1973489bea864b013cd3f6
tommy19970714 <toshiki0714@icloud.com>
2017-10-27 05:15:34 +0900

    comを選択できるように修正

--- Assets/Script/StartMenuManager.cs
+++ Assets/Script/StartMenuManager.cs
@@ -25,32 +25,41 @@ public class StartMenuManager : MonoBehaviour {
 			portList.AddRange (ports);
 			dropdown.AddOptions(portList);
             for(int i=0;i<portList.Count;i++) {
+                Debug.Log(portList[i]);
+                Debug.Log(PlayerPrefs.GetString("serialCom"));
                 if(portList[i] == PlayerPrefs.GetString("serialCom")) {
                     dropdown.value = i;
+                    Debug.Log("kokokoko");
+                    Debug.Log("kokokoko");
                 }
             }
-		}
+
+            dropdown.onValueChanged.AddListener(delegate
+            {
+                selectvalue(dropdown);
+            });
+        }
 
 		ipAddressLabel.text = "IPAddress: " + UnityEngine.Network.player.ipAddress;
 		portInputField.text = PlayerPrefs.GetInt ("realtimeUdpPort", 22227).ToString();
         hostIpAddressField.text = PlayerPrefs.GetString("hostIpAddress", "192.168.1.240");
 	}
-	
-	public void OnClickHostButton() {
+
+    private void selectvalue(Dropdown gdropdown)
+    {
+        PlayerPrefs.SetString("serialCom", gdropdown.options[gdropdown.value].text);
+        PlayerPrefs.Save();
+        Debug.Log(dropdown.options[gdropdown.value].text);
+    }
+
+    public void OnClickHostButton() {
 		SceneManager.LoadScene ("ResetMotionSimulator");
 	}
 
 	public void OnClickClientButton() {
 		SceneManager.LoadScene ("Client");
 	}
-
-	public void ValueChnagedDropDown(int value) {
-		PlayerPrefs.SetString("serialCom", dropdown.options[value].text);
-		PlayerPrefs.Save ();
-        Debug.Log(dropdown.options[value].text);
-        Debug.Log("dropdown change");
-	}
-
+    
 	public void ValueChangedPortField() {
 		PlayerPrefs.SetInt ("replayUdpPort", int.Parse(portInputField.text));
 		PlayerPrefs.Save ();
